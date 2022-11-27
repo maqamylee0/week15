4b73e5378b75f0187a1e4611115a1492b27c1195
maqamylee0 <maqamylee0@gmail.com>
2022-11-27 21:26:20 +0300

    almost

--- lib/billing.dart
+++ lib/billing.dart
@@ -1,4 +1,5 @@
 
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -23,12 +24,15 @@ class Billing extends StatelessWidget {
 
         ),
         Container(
-          padding: const EdgeInsets.fromLTRB(40, 60, 40, 50),
+          padding: const EdgeInsets.fromLTRB(40, 80, 40, 50),
           width: MediaQuery.of(context).size.width,
           height:MediaQuery.of(context).size.height,
-          color: Colors.transparent,
+
           child:Container(
-            color: Colors.purple[200],
+            decoration:  BoxDecoration(
+                color: Colors.purple[200],
+                borderRadius: BorderRadius.all(Radius.circular(14))
+            ),
             child: Column(
               children: [
                 Container(
@@ -52,9 +56,48 @@ class Billing extends StatelessWidget {
                 ),
                 Container(
                   padding: EdgeInsets.all(30),
-                  child: const Text("START UP",style: TextStyle(color: Colors.white,
-                      fontSize: 33,decoration: TextDecoration.none),),
+                  child: const Text("STARTUP",style: TextStyle(color: Colors.white,
+                      fontSize: 30,decoration: TextDecoration.none),),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height*0.25,
+                  child: Column(
+
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text("Disk Space - 200GB",style: TextStyle(fontSize: 15,color: Colors.white,decoration: TextDecoration.none),),
+                      Text("Bandwidth - 30GB",style: TextStyle(fontSize: 15,color: Colors.white,decoration: TextDecoration.none),),
+                      Text("Databases - 03",style: TextStyle(fontSize: 15,color: Colors.white,decoration: TextDecoration.none),),
+                      Text("E-mail Accounts - 10",style: TextStyle(fontSize: 15,color: Colors.white,decoration: TextDecoration.none),),
+                      Text("24/7 support - no",style: TextStyle(fontSize: 15,color: Colors.white,decoration: TextDecoration.none),),
+                      Text("E-mail support - yes",style: TextStyle(fontSize: 15,color: Colors.white,decoration: TextDecoration.none),)
+                    ],
+                  ),
+                ) ,
+                SizedBox(
+                  height: 50,
+                ),
+                SizedBox(
+                  width: 170,
+                  height: 45,
+                  child:  TextButton(
+                      child: Text(
+                          "SIGN UP".toUpperCase(),
+                          style: TextStyle(fontSize: 14,color: Colors.purple)
+                      ),
+                      style: ButtonStyle(
+                          padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                              )
+                          )
+                      ),
+                      onPressed: () => null
+                  ),
                 )
+
               ],
             ),
           ) ,
